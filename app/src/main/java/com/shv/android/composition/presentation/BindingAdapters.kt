f067dc1f6c8ee8731205f24f5fbb336896afb2bf
Sergey <frostfreerun@gmail.com>
2022-12-21 20:55:58 +0300

    Added Data Binding for GameFragment.kt & Data Binding Adapters, OnClickListener with interface

--- app/src/main/java/com/shv/android/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/com/shv/android/composition/presentation/BindingAdapters.kt
@@ -1,10 +1,22 @@
 package com.shv.android.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.shv.android.composition.R
 import com.shv.android.composition.domain.entity.GameResult
+
+
+interface OnOptionClickLister {
+
+    fun onOptionClick(option: Int)
+}
+
+//GameFinishedFragment Data Binding
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -54,4 +66,47 @@ private fun getResultImage(winState: Boolean): Int {
         R.drawable.ic_smile
     else
         R.drawable.ic_sad
+}
+
+//GameFragment Data Binding
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, sum: Int) {
+    textView.text = sum.toString()
+}
+
+@BindingAdapter(value = ["progress", "secondaryProgress"], requireAll = true)
+fun bindProgressState(progressBar: ProgressBar, progress: Int, progressSecondary: Int) {
+    progressBar.setProgress(progress, true)
+    progressBar.secondaryProgress = progressSecondary
+}
+
+@BindingAdapter("setColorText")
+fun bindColorText(textView: TextView, enoughCount: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enoughCount))
+}
+
+private fun getColorByState(context: Context, state: Boolean): Int {
+    val colorResId = if (state)
+        android.R.color.holo_green_light
+    else
+        android.R.color.holo_red_light
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("setColorProgressBar")
+fun bindColorProgressBar(progressBar: ProgressBar, enoughPercent: Boolean) {
+    progressBar.progressTintList = ColorStateList.valueOf(
+        getColorByState(
+            progressBar.context,
+            enoughPercent
+        )
+    )
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickLister) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
 }
--- app/src/main/java/com/shv/android/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/shv/android/composition/presentation/GameFragment.kt
@@ -1,21 +1,15 @@
 package com.shv.android.composition.presentation
 
-import android.content.res.ColorStateList
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.shv.android.composition.R
 import com.shv.android.composition.databinding.FragmentGameBinding
 import com.shv.android.composition.domain.entity.GameResult
-import com.shv.android.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
 
@@ -33,19 +27,6 @@ class GameFragment : Fragment() {
         ViewModelProvider(this, gameViewModelFactory)[GameViewModel::class.java]
     }
 
-    private val tvOptions by lazy {
-        mutableListOf<TextView>().apply {
-            with(binding) {
-                add(tvOption1)
-                add(tvOption2)
-                add(tvOption3)
-                add(tvOption4)
-                add(tvOption5)
-                add(tvOption6)
-            }
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -57,69 +38,15 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.viewModel = gameViewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         observeViewModels()
-        setupOnClickListenersToOptions()
-
     }
 
     private fun observeViewModels() {
-        with(binding) {
-            gameViewModel.formattedTime.observe(viewLifecycleOwner) {
-                tvTimer.text = it
-            }
-
-            gameViewModel.question.observe(viewLifecycleOwner) {
-                tvSum.text = it.sum.toString()
-                tvVisibleNumber.text = it.visibleNumber.toString()
-
-                for (i in 0 until tvOptions.size) {
-                    tvOptions[i].text = it.listOptions[i].toString()
-                }
-            }
-
-            gameViewModel.progressAnswers.observe(viewLifecycleOwner) {
-                tvCountRightAnswers.text = it
-            }
-
-            gameViewModel.percentRightAnswers.observe(viewLifecycleOwner) {
-                pbProgress.setProgress(it, true)
-            }
-
-            gameViewModel.enoughCountOfRightAnswers.observe(viewLifecycleOwner) {
-                tvCountRightAnswers.setTextColor(getColorByState(it))
-            }
-
-            gameViewModel.enoughPercentOfRightAnswers.observe(viewLifecycleOwner) {
-                val color = getColorByState(it)
-                pbProgress.progressTintList = ColorStateList.valueOf(color)
-            }
-
-            gameViewModel.minPercent.observe(viewLifecycleOwner) {
-                pbProgress.secondaryProgress = it
-            }
-
-            gameViewModel.gameResult.observe(viewLifecycleOwner) {
-                launchGameFinishedFragment(it)
-            }
+        gameViewModel.gameResult.observe(viewLifecycleOwner) {
+            launchGameFinishedFragment(it)
         }
-    }
-
-
-    private fun setupOnClickListenersToOptions() {
-        for (tvOption in tvOptions) {
-            tvOption.setOnClickListener {
-                gameViewModel.chooseAnswer(tvOption.text.toString().toInt())
-            }
-        }
-    }
-
-    private fun getColorByState(state: Boolean): Int {
-        val colorResId = if (state)
-            android.R.color.holo_green_light
-        else
-            android.R.color.holo_red_light
-        return ContextCompat.getColor(requireContext(), colorResId)
     }
 
     private fun launchGameFinishedFragment(gameResult: GameResult) {
